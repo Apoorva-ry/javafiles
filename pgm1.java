83148cc28d320643b01fc2658aae59b11075063b
Apoorva-ry <apoorvayaligar33@gmail.com>
2025-04-11 09:57:06 +0530

    save this

---
+++ pgm1.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+public class squareNumber{
+    public static void main(String[] args){
+        scanner scanner = new 
+        scanner(System.in);
+        system.out.print("Enter a number:");
+        int num=scanner.nextInt();
+        scanner.close();
+        int square=new*num;
+        system.out.println("Square of "+num+"is:"+square);
+    }
+}
